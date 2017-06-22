025a3440b46cea3d729ada2d4850e255d93c744e
Christian Dietz <dietzc85@googlemail.com>
2017-06-21 17:32:16 -0700

    Initial public release

---
+++ README.md
@@ -0,0 +1,10 @@
+# KNIME Image Processing - Deep Learning Extension
+
+### About
+This plug-in adds support for images from [KNIME Image
+Processing](https://www.knime.com/community/image-processing) to the
+KNIME Deep Learning extension.
+
+### License
+KNIME Image Processing - Deep Learning Extension is available under the same
+[License](https://www.knime.org/downloads/full-license) as KNIME.
---
+++ org.knime.knip.dl/src/org/knime/ip/dl/DLImgPlusValueToDoubleTensorConverterFactory.java
@@ -0,0 +1,118 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * -------------------------------------------------------------------
+ *
+ */
+
+package org.knime.ip.dl;
+
+import java.util.List;
+import java.util.OptionalLong;
+
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.ExtensibleUtilityFactory;
+import org.knime.dl.core.data.DLWritableDoubleBuffer;
+import org.knime.dl.core.data.convert.DLDataValueToTensorConverter;
+import org.knime.dl.core.data.convert.DLDataValueToTensorConverterFactory;
+import org.knime.knip.base.data.img.ImgPlusValue;
+
+import net.imglib2.Cursor;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.basictypeaccess.array.DoubleArray;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.DoubleType;
+
+/**
+ * @author Christian Dietz, KNIME, Konstanz, Germany
+ * @author Marcel Wiedenmann, KNIME, Konstanz, Germany
+ */
+public class DLImgPlusValueToDoubleTensorConverterFactory<T extends RealType<T>>
+		implements DLDataValueToTensorConverterFactory<ImgPlusValue, DLWritableDoubleBuffer> {
+
+	@Override
+	public String getName() {
+		return ((ExtensibleUtilityFactory) ImgPlusValue.UTILITY).getName();
+	}
+
+	@Override
+	public Class<ImgPlusValue> getSourceType() {
+		return ImgPlusValue.class;
+	}
+
+	@Override
+	public Class<DLWritableDoubleBuffer> getBufferType() {
+		return DLWritableDoubleBuffer.class;
+	}
+
+	@Override
+	public OptionalLong getDestCount(final List<DataColumnSpec> spec) {
+		return OptionalLong.empty();
+	}
+
+	@Override
+	public DLDataValueToTensorConverter<ImgPlusValue, DLWritableDoubleBuffer> createConverter() {
+		return (inputs, output) -> {
+			for (final ImgPlusValue input : inputs) {
+				final Img<T> img = input.getImgPlus().getImg();
+				final double[] out;
+				if (input.getImgPlus().getImg() instanceof ArrayImg && img.firstElement() instanceof DoubleType) {
+					out = ((DoubleArray) ((ArrayImg) img).update(null)).getCurrentStorageArray();
+				} else {
+					if (img.size() >= Integer.MAX_VALUE) {
+						throw new IllegalArgumentException(
+								"Can't process images with more than Integer.MAX_VALUE pixels, yet.");
+					}
+					// TODO can be parallelized
+					// TODO consider iteration order
+					out = new double[(int) img.size()];
+					final Cursor<T> c = img.cursor();
+					for (int i = 0; i < out.length; i++) {
+						out[i] = c.next().getRealDouble();
+					}
+				}
+				output.getBuffer().putAll(out);
+			}
+		};
+	}
+}
