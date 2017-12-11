b2682728cb7b6d7f5425612be6b3ae37486aaeab
Adrian Nembach <adrian.nembach@knime.com>
2017-12-11 10:47:44 +0100

    AP-8494: Implement dimension mapping for depth and time
    AP-8494 (DL Framework: Auto-mapping of dimensions for multi-dim inputs)

--- org.knime.knip.dl/src/org/knime/ip/dl/DLDimensionMapper.java
+++ org.knime.knip.dl/src/org/knime/ip/dl/DLDimensionMapper.java
@@ -1,5 +1,6 @@
 package org.knime.ip.dl;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,22 +27,24 @@ class DLDimensionMapper {
 	}
 	
 	/**
-	 * This method must always be consistent with SUPPORTED_AXES
-	 * 
 	 * @param dimOrdering the dimension order the deep learning backend expects
 	 * @return a map that maps the axes of a ImgPlus to the order the backend expects
 	 */
 	private static Map<AxisType, Integer> createDimMapping(DLDimensionOrder dimOrdering) {
 		Map<AxisType, Integer> dimMapping = new HashMap<>();
 		switch (dimOrdering) {
-		case BCHW:
+		case BTCDHW:
 			dimMapping.put(Axes.X, 0);
 			dimMapping.put(Axes.Y, 1);
-			dimMapping.put(Axes.CHANNEL, 2);
+			dimMapping.put(Axes.Z, 2);
+			dimMapping.put(Axes.CHANNEL, 3);
+			dimMapping.put(Axes.TIME, 4);
 			return dimMapping;
-		case BHWC:
+		case BTDHWC:
 			dimMapping.put(Axes.X, 1);
 			dimMapping.put(Axes.Y, 2);
+			dimMapping.put(Axes.Z, 3);
+			dimMapping.put(Axes.TIME, 4);
 			dimMapping.put(Axes.CHANNEL, 0);
 			return dimMapping;
 		default:
@@ -58,11 +61,11 @@ class DLDimensionMapper {
 	public <T extends RealType> ImgPlus<T> mapDimensionsFromDL(final ImgPlus img) {
 		// TODO: meta data for the output
 		switch (m_dimOrder) {
-		case BCHW:
+		case BTCDHW:
 			return img;
-		case BHWC:
+		case BTDHWC:
 			int[] mapping = new int[] {2, 0, 1};
-			return new ImgPlus<T>(ImgView.wrap(DimSwapper.swap(img, mapping), img.factory()));
+			return new ImgPlus<>(ImgView.wrap(DimSwapper.swap(img, mapping), img.factory()));
 		default:
 			throw new IllegalStateException("Unsupported dimension order '" + m_dimOrder + "'.");
 		}
@@ -70,28 +73,42 @@ class DLDimensionMapper {
 	
 	private int[] inferMapping(ImgPlus<?> img) {
 		int[] mapping = new int[img.numDimensions()];
+		// keeps track where we put the corresponding dimension
+		int[] pos = new int[5];
+		// -1 indicates that we haven't seen the dimension yet
+		Arrays.fill(pos, -1);
+		// recognize img dimensions
 		for (int d = 0; d < img.numDimensions(); d++) {
 			AxisType a = img.axis(d).type();
 			Integer newIdx = m_dimMapping.get(a);
 			if (newIdx == null) {
 				throw new IllegalArgumentException("The axis type '" + a + "' is currently not supported.");
 			}
-			mapping[d] = newIdx;
+			pos[newIdx] = d;
+		}
+		int counter = 0;
+		// fill mapping according to dimension priority
+		for (int d = 0; d < pos.length; d++) {
+			if (pos[d] == -1) {
+				continue;
+			}
+			mapping[pos[d]] = counter++;
 		}
 		return mapping;
 	}
 	
 	public long[] getKNIPShape(final long[] dlShape) {
 		switch (m_dimOrder) {
-		case BCHW:
+		case BTCDHW:
 			return dlShape;
-		case BHWC:
+		case BTDHWC:
 			if (dlShape.length == 2) {
 				return dlShape;
 			} else if (dlShape.length == 3) {
 				long[] knipShape = new long[] {dlShape[2], dlShape[1], dlShape[0]};
 				return knipShape;
 			}
+			// TODO: Figure out how to handle higher dimensional output (Time vs. Depth)
 			throw new IllegalArgumentException("Shapes of rank " + dlShape.length + " are not supported, yet.");
 		default:
 			// not gonna happen because the constructor would already crash
